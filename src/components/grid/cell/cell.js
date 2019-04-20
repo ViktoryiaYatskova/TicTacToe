aa3468c4a7c7d89fd1d570b36fb8111a04b01dc9
Viktoria Yatskova <viktoria@MacBook-Pro-Viktoria.local>
2019-04-20 21:29:03 +0300

    #7 use svg for cell content

--- src/components/grid/cell/cell.js
+++ src/components/grid/cell/cell.js
@@ -9,17 +9,58 @@ const Cell = ({ cellState, coordinates, isWinCell, cellSize, playerTools }) => {
                     ${cellState === EMPTY_CELL ? 'empty-cell' : ''} 
                     ${isWinCell ? 'win-cell' : ''}`;
     const cellContent = playerTools[cellState] || '';
-    return (<button
-        style={{
-            color: !isWinCell && getToolColour(cellState),
-            width: cellSize + CELL_SIZE_UNIT,
-            height: cellSize + CELL_SIZE_UNIT
-        }}
+    const style = {
+        color: !isWinCell && getToolColour(cellState),
+        width: cellSize + CELL_SIZE_UNIT,
+        height: cellSize + CELL_SIZE_UNIT
+    };
+    const ElementTag =
+        cellContent === 'X' ? Cross :
+            cellContent === 'O' ? Nought :
+                'button';
+
+    return (<ElementTag
+        style={style}
         className={classes}
         row={coordinates.row}
         column={coordinates.column}
         key={`${coordinates.row}-${coordinates.column}`}
-    >{cellContent}</button>);
+    />);
 };
+
+const Cross = (props) => (
+    <svg
+        {...props}
+        viewBox={`0 0 100 100`}
+        xmlns="http://www.w3.org/2000/svg"
+        xlink="http://www.w3.org/1999/xlink"
+        version="1.1"
+        preserveAspectRatio="xMidYMid meet"
+    >
+        <path d="M100 100L0 0"
+            opacity="1" fillOpacity="0" stroke={props.style.color}
+            strokeWidth="5" strokeOpacity="1"
+        />
+        <path d="M0 100L100 0"
+            opacity="1" fillOpacity="0" stroke={props.style.color}
+            strokeWidth="5" strokeOpacity="1"
+        />
+    </svg>
+);
+
+const Nought = (props) => (
+    <svg
+        {...props}
+        viewBox={`0 0 100 100`}
+        xmlns="http://www.w3.org/2000/svg"
+        xlink="http://www.w3.org/1999/xlink"
+        version="1.1"
+        preserveAspectRatio="xMidYMid meet"
+    >
+        <path d="M 97 47 A 15 15 0 1 1 3 48 A 16 15 0 1 1 97 47 Z"
+            opacity="1" fillOpacity="0" stroke={props.style.color} strokeWidth="5" strokeOpacity="1"
+        />
+    </svg>
+);
 
 export default Cell;
